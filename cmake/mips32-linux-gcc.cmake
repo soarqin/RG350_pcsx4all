eb455c48eefb5a1424474e5a061f90b6b5d5279c
Soar Qin <soarchin@gmail.com>
2019-11-12 00:57:24 +0800

    updated Makefile, do not use flo due to bug found in PocketSNES compiling, fixed cmake toolchain file

--- cmake/mips32-linux-gcc.cmake
+++ cmake/mips32-linux-gcc.cmake
@@ -4,7 +4,7 @@ if("${CROSS}" STREQUAL "")
   set(CROSS mipsel-linux-)
 endif ()
 
-set(MIPS_FLAGS "${MIPS_FLAGS} -mips32r2 -DSHMEM_MIRRORING -mno-abicalls -fno-PIC -mno-check-zero-division")
+set(MIPS_CFLAGS "${MIPS_CFLAGS} -mips32r2 -fno-PIC -mno-check-zero-division -mplt -mno-shared -ffast-math -DGCW_ZERO")
 
 if("${MODEL}" STREQUAL "rs97")
   set(MIPS_FLAGS "${MIPS_FLAGS} -DRS97 -DGCW_ZERO")
@@ -39,9 +39,10 @@ set(CMAKE_OBJCOPY      "${TOOLCHAIN_BINARY_DIR}${CROSS}objcopy${TOOL_OS_SUFFIX}"
 set(CMAKE_OBJDUMP      "${TOOLCHAIN_BINARY_DIR}${CROSS}objdump${TOOL_OS_SUFFIX}" CACHE PATH "objdump")
 set(CMAKE_RANLIB       "${TOOLCHAIN_BINARY_DIR}${CROSS}ranlib${TOOL_OS_SUFFIX}"  CACHE PATH "ranlib")
 
-set(CMAKE_C_FLAGS          "${CMAKE_C_FLAGS} ${MIPS_CFLAGS}"     CACHE STRING "C flags")
-set(CMAKE_CXX_FLAGS        "${CMAKE_CXX_FLAGS} ${MIPS_CXXFLAGS}" CACHE STRING "C++ flags")
+set(CMAKE_C_FLAGS          "${CMAKE_C_FLAGS} ${MIPS_CFLAGS}"                     CACHE STRING "C flags")
+set(CMAKE_CXX_FLAGS        "${CMAKE_CXX_FLAGS} ${MIPS_CFLAGS} ${MIPS_CXXFLAGS}"  CACHE STRING "C++ flags")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--as-needed -Wl,--gc-sections" CACHE STRING "Executable linker flags")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -s"   CACHE STRING "Executable linker flags for Release")
 
 # No runtime cpu detect for mipsel-linux-gcc.
 set(CONFIG_RUNTIME_CPU_DETECT 0 CACHE BOOL "")
